6fc8af2805789f2f98be0f8e5f36f44cbed81835
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-09-16 15:11:50 +0800

    allow adding and deleting of todo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,40 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
+import { AppService } from './app.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Delete('delete-todo/:id')
+  delete(@Param('id') id: string, @Res() res: Response) {
+    this.appService.deleteTodo(id);
+
+    return res.send();
+  }
+
+  @Post('add-todo')
+  addTodo(@Body() body: CreateTodoDto, @Res() res: Response) {
+    const todo = this.appService.addTodo(body.text);
+
+    return res.render('partials/todo-item', {
+      layout: false,
+      text: todo.text,
+      id: todo.id,
+    });
+  }
+
   @Get()
-  getHello(@Res() res: Response) {
+  homePage(@Res() res: Response) {
     const todos = this.appService.getTodos();
     console.log(todos);
     return res.render('home', {
--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,9 @@ export type Todo = {
   isDone?: boolean;
 };
 
-const todos: Todo[] = [
+let id = 1;
+
+let todos: Todo[] = [
   {
     id: '1',
     text: 'todo1',
@@ -17,5 +19,21 @@ const todos: Todo[] = [
 export class AppService {
   getTodos(): Todo[] {
     return todos;
+  }
+
+  addTodo(text: string) {
+    id++;
+
+    const todo = {
+      id: id.toString(),
+      text,
+    };
+    todos.push(todo);
+
+    return todo;
+  }
+
+  deleteTodo(id: string) {
+    todos = todos.filter((el) => el.id !== id);
   }
 }
